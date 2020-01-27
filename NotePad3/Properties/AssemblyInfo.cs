16da28f159de4b68e39c577854e9c348a0100aeb
Pedro H. G. Souto <phgsouto@gmail.com>
2020-01-27 20:39:24 -0300

    Adding a better about box. Fixes #6.

--- NotePad3/Properties/AssemblyInfo.cs
+++ NotePad3/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NotePad3")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Simple (and purely educational) notepad clone")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("phgsouto https://github.com/phgsouto/")]
 [assembly: AssemblyProduct("NotePad3")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("This software is distributed under the MIT license ( https://opensource.org/licenses/MIT )")]
+[assembly: AssemblyTrademark("Source Code: https://github.com/phgsouto/Notepad3")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
